0771ebc915fb65b4083ffb3a1d72fc66c96c017e
Guille Pérez <guipetor@gmail.com>
2019-01-31 00:19:44 +0100

    week3 almost done

--- week3/week3-Embeddings.py
+++ week3/week3-Embeddings.py
@@ -22,12 +22,7 @@ def hits_count(dup_ranks, k):
     ######################################
     ######### YOUR CODE HERE #############
     ######################################
-    boolean_list = [rank_dup <= k for rank_dup in dup_ranks]
-    hitsk = [i for i, x in enumerate(boolean_list,1) if x]
-    if any(boolean_list):
-        return np.mean(hitsk)
-    else:
-        return 0
+    return np.mean([1 if rank_dup <= k else 0 for rank_dup in dup_ranks])
     
 #%%
 def test_hits():
@@ -92,3 +87,94 @@ def test_hits():
     
 
 print(test_hits())
+
+#%%
+
+def dcg_score(dup_ranks, k):
+    """
+        dup_ranks: list of duplicates' ranks; one rank per question; 
+                   length is a number of questions which we are looking for duplicates; 
+                   rank is a number from 1 to len(candidates of the question); 
+                   e.g. [2, 3] means that the first duplicate has the rank 2, the second one — 3.
+        k: number of top-ranked elements (k in DCG@k metric)
+
+        result: return DCG@k value for current ranking
+    """
+    ######################################
+    ######### YOUR CODE HERE #############
+    ######################################
+    hitsk = np.mean([1 if rank_dup <= k else 0 for rank_dup in dup_ranks])
+    
+    return np.mean(hitsk*1/(np.log2(1+np.array(dup_ranks))))
+    
+#%%
+        
+    
+    
+def test_dcg():
+    # *Evaluation example*
+    # answers — dup_i
+    answers = ["How does the catch keyword determine the type of exception that was thrown"]
+    
+    # candidates_ranking — the ranked sentences provided by our model
+    candidates_ranking = [["How Can I Make These Links Rotate in PHP", 
+                           "How does the catch keyword determine the type of exception that was thrown",
+                           "NSLog array description not memory address",
+                           "PECL_HTTP not recognised php ubuntu"]]
+    # dup_ranks — position of the dup_i in the list of ranks +1
+    dup_ranks = [candidates_ranking[i].index(answers[i]) + 1 for i in range(len(answers))]
+    
+    # correct_answers — the expected values of the result for each k from 1 to 4
+    correct_answers = [0, 1 / (np.log2(3)), 1 / (np.log2(3)), 1 / (np.log2(3))]
+    for k, correct in enumerate(correct_answers, 1):
+        if not np.isclose(dcg_score(dup_ranks, k), correct):
+            return "Check the function."
+    
+    # Other tests
+    answers = ["How does the catch keyword determine the type of exception that was thrown", 
+               "Convert Google results object (pure js) to Python object"]
+
+    # The first test: both duplicates on the first position in ranked list
+    candidates_ranking = [["How does the catch keyword determine the type of exception that was thrown",
+                           "How Can I Make These Links Rotate in PHP"], 
+                          ["Convert Google results object (pure js) to Python object",
+                           "WPF- How to update the changes in list item of a list"]]
+    dup_ranks = [candidates_ranking[i].index(answers[i]) + 1 for i in range(len(answers))]
+    correct_answers = [1, 1]
+    for k, correct in enumerate(correct_answers, 1):
+        if not np.isclose(dcg_score(dup_ranks, k), correct):
+            return "Check the function (test: both duplicates on the first position in ranked list)."
+        
+    # The second test: one candidate on the first position, another — on the second
+    candidates_ranking = [["How Can I Make These Links Rotate in PHP", 
+                           "How does the catch keyword determine the type of exception that was thrown"], 
+                          ["Convert Google results object (pure js) to Python object",
+                           "WPF- How to update the changes in list item of a list"]]
+    dup_ranks = [candidates_ranking[i].index(answers[i]) + 1 for i in range(len(answers))]
+    correct_answers = [0.5, (1 + (1 / (np.log2(3)))) / 2]
+    for k, correct in enumerate(correct_answers, 1):
+        if not np.isclose(dcg_score(dup_ranks, k), correct):
+            return "Check the function (test: one candidate on the first position, another — on the second)."
+        
+    # The third test: both candidates on the second position
+    candidates_ranking = [["How Can I Make These Links Rotate in PHP",
+                           "How does the catch keyword determine the type of exception that was thrown"], 
+                          ["WPF- How to update the changes in list item of a list",
+                           "Convert Google results object (pure js) to Python object"]]
+    dup_ranks = [candidates_ranking[i].index(answers[i]) + 1 for i in range(len(answers))]
+    correct_answers = [0, 1 / (np.log2(3))]
+    for k, correct in enumerate(correct_answers, 1):
+        if not np.isclose(dcg_score(dup_ranks, k), correct):
+            return "Check the function (test: both candidates on the second position)."
+
+    return "Basic test are passed."
+
+#%%
+    
+
+print(test_dcg())
+
+
+
+
+
