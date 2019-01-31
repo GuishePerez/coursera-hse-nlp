13aae7c4749047453525628a0f902e1335cca70d
Guille Pérez <guipetor@gmail.com>
2019-01-31 23:48:58 +0100

    week3 exercices done

--- week3/week3-Embeddings.py
+++ week3/week3-Embeddings.py
@@ -6,6 +6,44 @@ Created on Wed Jan 30 00:44:33 2019
 """
 
 import numpy as np
+import gensim
+from sklearn.metrics.pairwise import cosine_similarity
+
+#%%
+#wv_embeddings = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin',binary = True,limit = 500000)
+
+#%%
+
+def question_to_vec(question, embeddings, dim=300):
+    """
+        question: a string
+        embeddings: dict where the key is a word and a value is its' embedding
+        dim: size of the representation
+
+        result: vector representation for the question
+    """
+    ######################################
+    ######### YOUR CODE HERE #############
+    ######################################
+    word_tokens = question.split(" ")
+    question_len = len(word_tokens)
+    question_mat = np.zeros((question_len,dim), dtype = np.float32)
+    
+    for idx, word in enumerate(word_tokens):
+        if word in embeddings:
+            question_mat[idx,:] = embeddings[word]
+            
+    # remove zero-rows which stand for OOV words       
+    question_mat = question_mat[~np.all(question_mat == 0, axis = 1)]
+    
+    # Compute the mean of each word along the sentence
+    if question_mat.shape[0] > 0:
+        vec = np.array(np.mean(question_mat, axis = 0), dtype = np.float32).reshape((1,dim))
+    else:
+        vec = np.zeros((1,dim), dtype = np.float32)
+        
+    return vec
+
 
 #%%
 
@@ -103,7 +141,7 @@ def dcg_score(dup_ranks, k):
     ######################################
     ######### YOUR CODE HERE #############
     ######################################
-    hitsk = np.mean([1 if rank_dup <= k else 0 for rank_dup in dup_ranks])
+    hitsk = np.array([1 if rank_dup <= k else 0 for rank_dup in dup_ranks])
     
     return np.mean(hitsk*1/(np.log2(1+np.array(dup_ranks))))
     
@@ -174,6 +212,63 @@ def test_dcg():
 
 print(test_dcg())
 
+#%%
+
+def read_corpus(filename):
+    data = []
+    for line in open(filename, encoding='utf-8'):
+        data.append(line.strip().split('\t'))
+    return data
+
+#%%
+
+#validation = read_corpus('data/validation.tsv')
+
+#%%
+    
+def rank_candidates(question, candidates, embeddings, dim=300):
+    """
+        question: a string
+        candidates: a list of strings (candidates) which we want to rank
+        embeddings: some embeddings
+        dim: dimension of the current embeddings
+        
+        result: a list of pairs (initial position in the list, question)
+    """
+    
+    ######################################
+    ######### YOUR CODE HERE #############
+    ######################################
+    # vectorize the embeddings of question and candidates
+    emb_question = np.array(question_to_vec(question,embeddings,dim))
+    emb_candidates = np.squeeze(np.array([question_to_vec(candidate,embeddings,dim) for candidate in candidates], dtype = np.float32))
+    # Compute the cosine similarity btw question and each candidate
+    cos_list = [cosine_similarity(emb_question,emb_candidates)[0]]
+    # Reshape results
+    pairs_unsorted = [(pos,candidates[pos],cos_list[pos]) for pos in range(len(candidates))]
+    pairs_sorted = sorted(pairs_unsorted,key = lambda x: x[2], reverse = True)
+    
+    return [(pos,cand) for pos,cand,_ in pairs_sorted]
+    
+
+#%%
+    
+wv_ranking = []
+for line in validation:
+    q, *ex = line
+    ranks = rank_candidates(q, ex, wv_embeddings)
+    wv_ranking.append([r[0] for r in ranks].index(0) + 1)
+    
+#%%
+    
+for k in [1, 5, 10, 100, 500, 1000]:
+    print("DCG@%4d: %.3f | Hits@%4d: %.3f" % (k, dcg_score(wv_ranking, k), k, hits_count(wv_ranking, k)))
+    
+#%%
+    
+for line in validation[:3]:
+    q, *examples = line
+    print(q, *examples[:3])
 
 
 
